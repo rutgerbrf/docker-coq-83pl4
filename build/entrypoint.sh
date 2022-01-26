8d6d8a69268ce8807d0afb66dec3aadedfca9784
Rutger Broekhoff <rutger@viasalix.nl>
2022-01-27 00:42:49 +0100

    Add icon theme, allow user to install packages

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "$USER_NAME:x:$USER_UID:$USER_GID:$USER_REAL_NAME:$HOME:/bin/bash" >> /etc/passwd
+echo "#$USER_UID ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 cmd="${1:-coqide}"
 shift
