1ef643516be6ae60fdbf88aca54b3cc067cfc07b
Rutger Broekhoff <rutger@viasalix.nl>
2022-01-26 23:37:32 +0100

    Fix passing USER_REAL_NAME, no warnings when running coqide anymore

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ trap "rm -f $XAUTH" EXIT
 USER_REAL_NAME="$(grep "^$(id -un):x:$(id -u):$(id -g):" /etc/passwd | cut -d: -f5 -)"
 
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -ti --net host -e HOME -e DISPLAY -e XAUTHORITY=$XAUTH -e USER_NAME=$(id -un) -e USER_UID=$(id -u) -e USER_GID=$(id -g) -e USER_REAL_NAME -v $XAUTH:$XAUTH -v $XSOCK:$XSOCK -v $HOME:$HOME ghcr.io/rutgerbrf/docker-coq-83pl4:main $@
+docker run -ti --net host -e HOME -e DISPLAY -e XAUTHORITY=$XAUTH -e USER_NAME=$(id -un) -e USER_UID=$(id -u) -e USER_GID=$(id -g) -e USER_REAL_NAME="$USER_REAL_NAME" -v $XAUTH:$XAUTH -v $XSOCK:$XSOCK -v $HOME:$HOME ghcr.io/rutgerbrf/docker-coq-83pl4:main $@
